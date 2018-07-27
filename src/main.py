1b99e9c18c06c145ffef2d68be5b4554a74abefa
zlikar <ziga.likar1@gmail.com>
2018-07-27 14:33:01 +0200

    Cloud NLP

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
+import json
 import numpy as np
 import keras as K
 from models.cnn import CNN
 from models.rnn import RNN
 import modules.util as util
 import modules.preprocess as pp
+import modules.entities as ent
+from keras.preprocessing.text import Tokenizer
 
-import matplotlib.pyplot as plt
-
+config = json.loads(open('config.json', encoding='utf-8', errors='ignore').read())
 sentences_training = open(r'datasets/sentences_training.txt', encoding='utf-8', errors='ignore').read().split('\n')
 sentences_test = open(r'datasets/sentences_test.txt', encoding='utf-8', errors='ignore').read().split('\n')
 
@@ -23,16 +25,9 @@ _, _, test_data, test_targets = pp.preprocess_data(sentences_test)
 rnn = RNN()
 rnn.init(num_words, embedding_matrix)
 r_rnn = rnn.fit(data, targets)
-#util.visualize_data(r_rnn)
+util.visualize_data(r_rnn)
 
-#tokens = myTokenizer.texts_to_matrix(test_data)
-#predictions = rnn.predict(np.array(tokens))
-
-error_sum = 0
-predictions = rnn.predict(np.array(test_data))
-
-for i in np.arange(0, 1, 0.05):
-    util.apply_threshold(predictions.copy(), test_targets, i)
+# predictions = rnn.predict(np.array(test_data))
 
 # print(len(predictions))
 # print(predictions)
