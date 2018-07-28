05a1fe90a3f5a34b133d51388d786aeb26d6b9b8
zlikar <ziga.likar1@gmail.com>
2018-07-28 12:28:08 +0200

    Changed task model

--- src/models/task.py
+++ src/models/task.py
@@ -1,9 +1,8 @@
 class Task:
 
-    def __init__(self, title, description, location_list, person_list, date_list, time_list):
+    def __init__(self, title, description, location_list, person_list, datetime_list):
         self.title = title
         self.description = description
         self.location_list = location_list
         self.person_list = person_list
-        self.date_list = date_list
-        self.time_list = time_list
+        self.datetime_list = datetime_list
--- src/modules/extract.py
+++ src/modules/extract.py
@@ -5,6 +5,8 @@ import modules.preprocess as pp
 
 from models.task import Task
 
+from dateutil.parser import parse
+
 """ Gets called when a user receives an email -> extracts the task """
 def mail_callback(nn, sentences, content):
     _, _, inputs, _ = pp.preprocess_data(sentences)
@@ -22,8 +24,7 @@ def mail_callback(nn, sentences, content):
             # TODO: preveriti, če Google podpira te entityje
             location_list = []
             person_list = []
-            date_list = []
-            time_list = []
+            datetime_list = []
             for key, value in entities.items():
                 if key == 'LOCATION':
                     location_list.append(value)
@@ -31,14 +32,15 @@ def mail_callback(nn, sentences, content):
                     person_list.append(value)
             
             # TODO: parse dates/times
+            datetime = parse(sentences[i])
+            print(datetime)
 
             task = Task(
                 title = sentences[i], # TODO: title iz keywordov
                 description = content,
                 location_list = location_list,
                 person_list = person_list,
-                date_list = date_list,
-                time_list = time_list)
+                datetime_list = datetime_list)
             
             tasks.append(task)
     
