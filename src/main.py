bug fix, precision-recall visualization

src/main.py
@@ -1,14 +1,14 @@
 import json
 
 from models.rnn import RNN
-from modules.util import visualize_data, preprocess_data
+from modules.util import visualize_data, preprocess_data, precision_recall_plot
 
 config = json.loads(open('config.json', encoding='utf-8', errors='ignore').read())
 sentences_training = open(r'datasets/sentences_training.txt', encoding='utf-8', errors='ignore').read().split('\n')
-# sentences_test = open(r'datasets/sentences_test.txt', encoding='utf-8', errors='ignore').read().split('\n')
+sentences_test = open(r'datasets/sentences_test.txt', encoding='utf-8', errors='ignore').read().split('\n')
 
 num_words, embedding_matrix, data, targets = preprocess_data(sentences_training)
-# _, _, test_data, test_targets = pp.preprocess_data(sentences_test)
+_, _, x_test, y_test = preprocess_data(sentences_test, test_data=True)
 
 # CNN
 # cnn = CNN()
@@ -21,6 +21,10 @@ rnn = RNN()
 rnn.init(num_words, embedding_matrix)
 r_rnn = rnn.fit(data, targets)
 visualize_data(r_rnn)
+
+y_pred = rnn.predict(x_test)
+
+precision_recall_plot(y_test, y_pred)
 
 # predictions = rnn.predict(np.array(test_data))
 

src/modules/util.py
@@ -14,6 +14,7 @@ MAX_VOCABULARY = config['MAX_VOCABULARY']
 
 class Glove:
     word2vec = None
+    embedding_matrix_loaded = False
     embedding_matrix = None
 
     @staticmethod
@@ -35,7 +36,7 @@ class Glove:
 
     @staticmethod
     def create_embedding_matrix(word2idx, num_words, EMBEDDING_DIM, recreate=False):
-        if Glove.embedding_matrix and not recreate:
+        if Glove.embedding_matrix_loaded and not recreate:
             return Glove.embedding_matrix
 
         Glove.embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
@@ -48,6 +49,7 @@ class Glove:
                 if embedding_vector is not None:
                     Glove.embedding_matrix[i] = embedding_vector
 
+        Glove.embedding_matrix_loaded = True
         return Glove.embedding_matrix
 
 
@@ -102,14 +104,6 @@ def precision_recall_plot(targets, predictions):
     plt.title('Average Precision={0:0.2f}'.format(
         average_precision))
     plt.show()
-
-
-def predict_on_test(model):
-    data = open(r'datasets/sentences_test.txt', encoding='utf-8', errors='ignore').read().split('\n')
-    x_test, y_test, _, _ = preprocess_data(data, test_data=True)
-
-    return model.predict(x_test)
-
 
 
 def apply_threshold(predictions, targets, threshold):
@@ -198,6 +192,7 @@ def clean_text(text):
     text = re.sub('\s+', ' ', text)
     text = text.strip()
     return text
+
 
 def preprocess_data(data, test_data=False):
     ## split dataset sentences into two arrays (inputs & outputs)